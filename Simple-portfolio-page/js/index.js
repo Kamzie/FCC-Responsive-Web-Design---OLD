9b864dc209e174c7b11c93ebd4f62107c262a565
Kamran Ramzan <38415153+Kamzie@users.noreply.github.com>
2021-10-26 14:55:59 +0100

    Update index.js

--- Simple-portfolio-page/js/index.js
+++ Simple-portfolio-page/js/index.js
@@ -2,3 +2,11 @@
       <a href="https://en-gb.facebook.com" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
       <a href="https://mobile.twitter.com/?lang=en" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
       <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a> */
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
